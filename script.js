aa527c8aed1d55277f55abd8bd2f0b66634e1b95
lokeshazad10 <lokeshazad10@gmail.com>
2024-09-19 18:53:21 +0530

    Update some features

--- script.js
+++ script.js
@@ -7,19 +7,3 @@ document.getElementById('project-nav').addEventListener("click", ()=>{
 document.getElementById('contact-nav').addEventListener("click", ()=>{
     document.querySelector('.contact').scrollIntoView({behavior: 'smooth'});
 })
-
-//making navbar fixed when scroll down
-window.addEventListener('scroll', function(){
-    var navbar = document.getElementById("header");
-
-    var scrollPostion = window.scrollY || document.documentElement.scrollTop;
-
-    if(scrollPostion>200){
-        navbar.classList.add('fixed');
-        navbar.style.backgroundColor = "#1d1d1d";
-    }else{
-        navbar.classList.remove('fixed');
-        navbar.style.backgroundColor = "";
-    }
-});
-
